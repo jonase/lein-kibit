3a6142f38cf7fc68906fe7b118b596bbdeef6297
Jonas Enlund <jonas.enlund@gmail.com>
2012-11-11 16:39:38 +0200

    Add dep on kibit

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject lein-kibit "0.0.5"
+(defproject lein-kibit "0.0.6"
   :description "kibit lein plugin"
   :url "https://github.com/jonase/lein-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[jonase/kibit "0.0.6"]]
   :eval-in-leiningen true)
