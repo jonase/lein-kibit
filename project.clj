89f60dd372389a8770fa631a14a50026c12e5ebd
Fraz <frazer.irving@mailonline.co.uk>
2013-09-25 15:58:21 +0100

    Pass source-paths to kibit.driver/run
    Rather than the complete project, also ups the version to 0.0.9-SNAPSHOT
    for lein-kibit and the dependency kibit for consistency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-kibit "0.0.8"
+(defproject lein-kibit "0.0.9-SNAPSHOT"
   :description "kibit lein plugin"
   :url "https://github.com/jonase/lein-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jonase/kibit "0.0.8"]]
+  :dependencies [[jonase/kibit "0.0.9-SNAPSHOT"]]
   :eval-in-leiningen true)
--- src/leiningen/kibit.clj
+++ src/leiningen/kibit.clj
@@ -2,9 +2,9 @@
   (:require [leiningen.core.eval :refer [eval-in-project]]))
 
 (defn ^:no-project-needed kibit
-  "I don't do a lot."
   [project & args]
-  (let [kibit-project '{:dependencies [[jonase/kibit "0.0.8"]]}
-        src `(kibit.driver/run '~project ~@args)
+  (let [kibit-project '{:dependencies [[jonase/kibit "0.0.9-SNAPSHOT"]]}
+        paths (or (:source-paths project) [(:source-path project)])
+        src `(kibit.driver/run '~paths ~@args)
         req '(require 'kibit.driver)]
     (eval-in-project kibit-project src req)))
