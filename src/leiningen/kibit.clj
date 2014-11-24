4eb476030cd049aaff1dcd7ffb5bf8b3260aa8c2
Daniel Compton <desk@danielcompton.net>
2014-11-24 14:45:04 +1300

    Add cljsbuild source-paths

--- src/leiningen/kibit.clj
+++ src/leiningen/kibit.clj
@@ -4,7 +4,11 @@
 (defn ^:no-project-needed kibit
   [project & args]
   (let [kibit-project '{:dependencies [[jonase/kibit "0.0.9-SNAPSHOT"]]}
-        paths (or (:source-paths project) [(:source-path project)])
+        paths (seq (disj (set (concat
+                                (:source-paths project)
+                                [(:source-path project)]
+                                (mapcat :source-paths (get-in project [:cljsbuild :builds]))))
+                         nil))
         src `(kibit.driver/external-run '~paths ~@args)
         req '(require 'kibit.driver)]
     (try (eval-in-project kibit-project src req)
