3249c96af7e65859cbf5337fa0ea12be34acdf1c
Daniel Compton <desk@danielcompton.net>
2015-04-20 19:57:45 +1200

    Bump kibit version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/jonase/lein-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jonase/kibit "0.1.1-SNAPSHOT"]]
+  :dependencies [[jonase/kibit "0.1.1"]]
   :eval-in-leiningen true)
