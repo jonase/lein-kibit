3204f08e5671bff211b5c600f18269de1f3aff7d
Daniel Compton <desk@danielcompton.net>
2015-04-20 19:11:54 +1200

    Bump snapshot to continue working

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-kibit "0.1.0"
+(defproject lein-kibit "0.1.1-SNAPSHOT"
   :description "kibit lein plugin"
   :url "https://github.com/jonase/lein-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jonase/kibit "0.1.0"]]
+  :dependencies [[jonase/kibit "0.1.1-SNAPSHOT"]]
   :eval-in-leiningen true)
