d5579371fd8f4524111aa54b631ef1da2293ee27
Daniel Compton <desk@danielcompton.net>
2015-08-29 17:48:11 +1200

    Simplify removing nils from paths argument

--- src/leiningen/kibit.clj
+++ src/leiningen/kibit.clj
@@ -4,12 +4,11 @@
 (defn ^:no-project-needed kibit
   [project & args]
   (let [kibit-project '{:dependencies [[jonase/kibit "0.1.2"]]}
-        paths (seq (disj (set (concat
-                                (:source-paths project)
-                                [(:source-path project)]
-                                (mapcat :source-paths (get-in project [:cljsbuild :builds]))
-                                (mapcat :source-paths (get-in project [:cljx :builds]))))
-                         nil))
+        paths (filter some? (concat
+                              (:source-paths project)
+                              [(:source-path project)]
+                              (mapcat :source-paths (get-in project [:cljsbuild :builds]))
+                              (mapcat :source-paths (get-in project [:cljx :builds]))))
         src `(kibit.driver/external-run '~paths ~@args)
         req '(require 'kibit.driver)]
     (try (eval-in-project kibit-project src req)
